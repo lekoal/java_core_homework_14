58ddacc82455b09ad9fa436e98ca6b13b497ed92
lekoal <coreglus@gmail.com>
2021-09-22 22:20:18 +0300

    Added both tasks + main work checking

--- src/main/java/ru/geekbrains/java_core_homework_14/SecondTask.java
+++ src/main/java/ru/geekbrains/java_core_homework_14/SecondTask.java
@@ -1,4 +1,18 @@
 package ru.geekbrains.java_core_homework_14;
 
 public class SecondTask {
+    public static boolean isHereOneAndFour(int[] myArrTaskTwo) {
+        int oneCount = 0;
+        int fourCount = 0;
+        for (int i : myArrTaskTwo) {
+            if (i == 1) {
+                oneCount++;
+            }
+            if (i == 4) {
+                fourCount++;
+            }
+        }
+        if (oneCount == 0 || fourCount == 0) return false;
+        else return (oneCount + fourCount) == myArrTaskTwo.length;
+    }
 }
